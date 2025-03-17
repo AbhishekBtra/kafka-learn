8645ae9eace275ba5b18fd3503882df4d8421616
Abhishek Batra <abatra002@gmail.com>
2025-03-17 20:36:25 +0530

    ..

--- src/org/java/com/learn/Main.java
+++ src/org/java/com/learn/Main.java
@@ -10,7 +10,7 @@ public class Main {
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-        for (int i=0; i<10; i++){
+        for (int i=0; i<20; i++){
             String message=String.format("Hello World ! %s",i);
             String key = String.format("Key %s",i);
             ProducerRecord<String,String> record = new ProducerRecord("quickstart-events",key,message);
